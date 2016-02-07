39c20d3182ef5e39367db0a3e75fb458d6960672
Pierre-Yves Poujade <pierre-yves@adgear.com>
2016-02-07 18:44:31 -0500

    Add guard, zeus

--- app/jobs/scrap_concerts_job.rb
+++ app/jobs/scrap_concerts_job.rb
@@ -4,6 +4,15 @@ class ScrapConcertsJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
-    Nokogiri::HTML(open("http://cheapthrills.ca/news.html"))
+    parse_html
+  end
+
+  private
+  def news_page_html
+    open('http://cheapthrills.ca/news.html')
+  end
+
+  def parse_html
+    news_page = Nokogiri::HTML(news_page_html)
   end
 end
--- spec/jobs/scrap_concerts_job_spec.rb
+++ spec/jobs/scrap_concerts_job_spec.rb
@@ -1,9 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe ScrapConcertsJob, type: :job do
-  it "gets the page's contents" do
-    stub_request(:get, 'cheapthrills.ca/news.html')
+  it 'gets the news page' do
+    stub = stub_request(:get, 'cheapthrills.ca/news.html')
+
     ScrapConcertsJob.perform_now
-    expect(WebMock).to have_requested(:get, 'cheapthrills.ca/news.html')
+
+    expect(stub).to have_been_requested.once
+
+    WebMock.reset!
+  end
+
+  it 'extracts concerts to the model' do
+    expected_concert = build(:concert, artist: 'YOUNG RIVAL / WE ARE MONROE', datetime: Time.new(2016, 1, 28, 20, 0, 0), venue: 'Belmont', price: 12.0)
+
+    VCR.use_cassette(:ct_news) do
+      ScrapConcertsJob.perform_now
+    end
+
+    expect(Concert.first).to eq(expected_concert)
   end
 end
