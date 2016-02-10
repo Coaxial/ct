acd8905c961662739fe2a0ff213a4b040de7c0fc
Coaxial <py@poujade.org>
2016-02-09 20:07:56 -0500

    Refactor tests, job

--- spec/models/concert_spec.rb
+++ spec/models/concert_spec.rb
@@ -1,20 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Concert, type: :model do
-  it "is not valid without an artist" do
-    subject = build(:concert, :no_artist)
-    expect(subject.valid?).to be false
-  end
-  it "is not valid without a datetime" do
-    subject = build(:concert, :no_datetime)
-    expect(subject.valid?).to be false
-  end
-  it "is not valid without a venue" do
-    subject = build(:concert, :no_venue)
-    expect(subject.valid?).to be false
-  end
-  it "is not valid without a price" do
-    subject = build(:concert, :no_price)
-    expect(subject.valid?).to be false
+  attributes = [:artist, :datetime, :venue, :price]
+  attributes.each do |attribute|
+    it "is not valid with a missing #{attribute}" do
+      trait = "no_#{attribute}".to_sym
+      concert = build(:concert, trait)
+
+      expect(concert.valid?).to be false
+    end
   end
 end
